b06e66b4d83326c457ff7cb2623253476087664d
ShadowWolf308 <levyvandervalk@gmail.com>
2021-12-10 15:26:51 +0100

    fixed slideshow

--- slideshow.js
+++ slideshow.js
@@ -12,16 +12,3 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000);
 }
-let ctx,canvas;
-canvas.width = 1000;
-canvas.height = 600;
-canvas = document.getElementById("slideshow");
-ctx = canvas.getContext("2d");
-function draw() {
-    ctx.beginPath();
-    ctx.fillStyle = "rgba(51,51,51,0.5)";
-    ctx.fillRect(0,canvas.height-18,canvas.width,18)
-    ctx.fill();
-    ctx.closePath()
-}
-draw();
