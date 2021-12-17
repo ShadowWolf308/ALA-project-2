b6a94dd9f2ecb0e5b5f74f5f3cef9acfc415d8e7
ShadowWolf308 <levyvandervalk@gmail.com>
2021-12-17 18:30:11 +0100

    advertenties

--- slideshow.js
+++ slideshow.js
@@ -1,6 +1,5 @@
 var slideIndex = 0;
 showSlides();
-
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("slides");
@@ -12,3 +11,13 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4500);
 }
+// window.onload = function() {
+//     var canvas,ctx 
+//     canvas = document.getElementsByTagName("body")
+//     ctx = canvas.getContext("2d")
+
+//     ctx.beginPath()
+//     ctx.fillStyle = rgba(0,0,0,0.5)
+//     ctx.fillRect(0,0,canvas.width,canvas.height)
+//     ctx.closePath()
+// }
